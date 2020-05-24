65b25491d0eaecb1ec77558abfeca8c23d12ab13
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-05-23 21:10:30 -0400

    updated sign in component

--- cracking-into-cooking/src/components/SignIn.js
+++ cracking-into-cooking/src/components/SignIn.js
@@ -1,22 +1,45 @@
-import React, {useState} from "react";
 import {Link} from "react-router-dom";
+import React, { Component } from 'react';
+import axios from 'axios';
 
 
-const SignIn = () => {
-    {/* create state hooks for the email password and error */}
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState(null);
-    {/* check to see if the email and password are correct */}
-    const signInWithEmailAndPasswordHandler = (event,email, password) => {
+export default class SignIn extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          users: [],
+          email:'',
+          password:'',
+          error:''
+        }
+
+        this.onChangeHandler = this.onChangeHandler.bind(this);
+        this.signInWithEmailAndPasswordHandler = this.signInWithEmailAndPasswordHandler.bind(this);
+  }
+
+    componentDidMount() {
+        {/* call the api on page load */}
+        this.callApi();
+}
+    async callApi() {
+        try {
+            const response = await axios.get('/walter_api/v2/users');
+            //console.log(response.data);
+            {/* store api data in state */}
+            this.setState({users:response.data});
+        } catch (e) {
+        console.log(e);
+        }
+    }
+    
+    signInWithEmailAndPasswordHandler = (event,email, password) => {
         event.preventDefault();
         // auth.signInWithEmailAndPassword(email, password).catch(error => {
         // setError("Error signing in with password and email!");
         //   console.error("Error signing in with password and email", error);
         // });
       };
-      {/* set the email and password into state */}
-      const onChangeHandler = (event) => {
+      onChangeHandler = (event) => {
           const {name, value} = event.currentTarget;
         
           if(name === 'userEmail') {
@@ -26,45 +49,45 @@ const SignIn = () => {
             setPassword(value);
           }
       };
-   {/* a form to handle the data */}
 
-  return (
-    <div className="main">
-      <h1>Sign In</h1>
-      <div >
-        {error !== null && <div>{error}</div>}
-        <form>
-          <label htmlFor="userEmail">
-            Email:
-          </label>
-          <input
-            type="email"
-            name="userEmail"
-            value = {email}
-            placeholder="test@gmail.com"
-            id="userEmail"
-            onChange = {(event) => onChangeHandler(event)}
-          />
-          <label htmlFor="userPassword">
-            Password:
-          </label>
-          <input
-            type="password"
-            name="userPassword"
-            value = {password}
-            placeholder="Your Password"
-            id="userPassword"
-            onChange = {(event) => onChangeHandler(event)}
-          />
-          <button onClick = {(event) => {signInWithEmailAndPasswordHandler(event, email, password)}}>
-            Sign in
-          </button>
-        </form>
-         <Link to="/SignUp">Don't have an account? Sign up here
-          </Link>
-      </div>
-    </div>
-  );
+      render() {
+        return (
+            <div className="main">
+              <h1>Sign In</h1>
+              <div >
+                {error !== null && <div>{error}</div>}
+                <form>
+                  <label htmlFor="userEmail">
+                    Email:
+                  </label>
+                  <input
+                    type="email"
+                    name="userEmail"
+                    value = {email}
+                    placeholder="test@gmail.com"
+                    id="userEmail"
+                    onChange = {(event) => onChangeHandler(event)}
+                  />
+                  <label htmlFor="userPassword">
+                    Password:
+                  </label>
+                  <input
+                    type="password"
+                    name="userPassword"
+                    value = {password}
+                    placeholder="Your Password"
+                    id="userPassword"
+                    onChange = {(event) => onChangeHandler(event)}
+                  />
+                  <button onClick = {(event) => {signInWithEmailAndPasswordHandler(event, email, password)}}>
+                    Sign in
+                  </button>
+                </form>
+                 <Link to="/SignUp">Don't have an account? Sign up here
+                  </Link>
+              </div>
+            </div>
+          );
+      }
+  
 };
-
-export default SignIn;
