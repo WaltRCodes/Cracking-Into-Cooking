06d80acdfee1809b04d684e072f257f834f9d7bc
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-05-25 22:25:55 -0400

    styled navbar

--- cracking-into-cooking/src/components/Navbar.js
+++ cracking-into-cooking/src/components/Navbar.js
@@ -3,8 +3,7 @@ import {Link} from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-        <p>Welcome!</p>
+    <div className="nav-bar">
         <Link to="/">Profile</Link>
         <Link to="/RecipeForm">Make new Recipe</Link>
         <Link to="/IngredientForm">Make new Ingredient</Link>
