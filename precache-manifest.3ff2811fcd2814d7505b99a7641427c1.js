f8309c8fa0016e6855bbd6f8799569726580695f
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-06-03 15:25:34 -0400

    Updates

---
+++ precache-manifest.3ff2811fcd2814d7505b99a7641427c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cba2e57ef0c210f0ff40d7a8229c1c1a",
+    "url": "/cracking-into-cooking/index.html"
+  },
+  {
+    "revision": "7437b7bf69a2a8f0b0ad",
+    "url": "/cracking-into-cooking/static/css/main.fc313730.chunk.css"
+  },
+  {
+    "revision": "fedadb7380b6ea80237b",
+    "url": "/cracking-into-cooking/static/js/2.acab4de5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cracking-into-cooking/static/js/2.acab4de5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7437b7bf69a2a8f0b0ad",
+    "url": "/cracking-into-cooking/static/js/main.9dd97acf.chunk.js"
+  },
+  {
+    "revision": "f103065445a0806f3056",
+    "url": "/cracking-into-cooking/static/js/runtime-main.c2e0b044.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cracking-into-cooking/static/media/logo.5d5d9eef.svg"
+  }
+]);
